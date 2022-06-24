cec8180bed80ccdeb57f81847e75a95a6da37c4a
Anatoly <tolya.cow@gmail.com>
2022-06-24 11:21:12 +0300

    4.3 + bounds + pdl

--- 4/utils.py
+++ 4/utils.py
@@ -175,3 +175,114 @@ def sgd_nesterov(batch_size, f, x, *, lr0, epoch, alpha):
         points[i] = x
     return points
 
+class Poly:
+    """"
+        c_0 + c_1 x + c_2 x^2 + .. + c_deg x^deg
+    """
+    def __init__(self, *, deg=2, coeffs=None):
+        if coeffs is not None:
+            self.coeffs = np.array(coeffs)
+        else:
+            self.coeffs = np.ones(deg+1)
+
+    def __call__(self, x):
+        return sum(c_i * x**i for (i, c_i) in enumerate(self.coeffs))
+
+    def grad(self, x):
+        return np.array([x**i for (i, c_ip1) in enumerate(self.coeffs[:])]).T[0]
+
+    def hessian(self, x):
+        return np.diag(list(x**i for (i, c_ip1) in enumerate(self.coeffs[:])))
+
+#%%
+
+def dogleg_method_step(grad_k, hessian_k, trust_radius):
+    hessian_k_inv = np.linalg.inv(hessian_k)
+    dx_newton = -np.matmul(hessian_k_inv, grad_k)
+    dx_newton_norm = np.linalg.norm(dx_newton)
+
+    if dx_newton_norm <= trust_radius:
+        return dx_newton
+
+    dx_steepest = - np.dot(grad_k, grad_k) / np.dot(grad_k, np.dot(hessian_k,grad_k)) * grad_k
+    dx_steepest_norm = np.linalg.norm(dx_steepest)
+
+    if dx_steepest_norm >= trust_radius:
+        return trust_radius * dx_steepest / dx_steepest_norm
+
+    diff = dx_newton - dx_steepest
+    dx_steepest_x_diff = np.matmul(dx_steepest.T, diff)
+    discriminant = dx_steepest_x_diff ** 2 - np.linalg.norm(diff) ** 2 * \
+                   (np.linalg.norm(dx_steepest) ** 2 - trust_radius ** 2)
+    tau = (-dx_steepest_x_diff + np.sqrt(discriminant)) / np.linalg.norm(diff) ** 2
+    return dx_steepest + tau * (dx_newton - dx_steepest)
+
+def trust_region_method(func, grad, hessian, x, tr0=1, tr_limit=2 ** 5, epoch=10, eta=0.1):
+    x_poly = Poly(coeffs=x)
+    points = np.zeros((epoch, len(x)))
+    points[0] = x_poly.coeffs
+    trust_radius = tr0
+    for i in range(1, epoch):
+        grad_k = grad(x_poly)
+        hessian_k = hessian(x_poly)
+        pk = dogleg_method_step(grad_k, hessian_k, trust_radius)
+
+        moved = Poly(coeffs=x_poly.coeffs + pk)
+
+        # Actual reduction.
+        act_red = sum(func(x_poly)**2) - sum(func(moved)**2)
+
+        # Predicted reduction.
+        # pred_red = -(np.dot(grad_k, pk) + 0.5 * np.dot(pk, np.dot(hessian_k , pk)))
+        pred_red = -(np.matmul(grad_k.T, pk) + 0.5 * np.matmul(pk.T, np.dot(hessian_k, pk)))
+        # print(f'{pred_red=}\n{act_red=}')
+        # print(f'{trust_radius = }')
+        # Rho.
+        if pred_red == 0.0:
+            rhok = 1e99
+        else:
+            rhok = act_red / pred_red
+
+        # Calculate the Euclidean norm of pk.
+        norm_pk = np.linalg.norm(pk)
+
+        # Rho is close to zero or negative, therefore the trust region is shrunk.
+        if rhok < 0.25:
+            trust_radius = 0.25 * trust_radius
+        else:
+            # Rho is close to one and pk has reached the boundary of the trust region, therefore the trust region is expanded.
+            if rhok > 0.75 and norm_pk == trust_radius:
+                trust_radius = min(2.0 * trust_radius, tr_limit)
+            else:
+                trust_radius = trust_radius
+
+        # Choose the position for the next iteration.
+        if rhok > eta:
+            x_poly = moved
+        else:
+            x_poly = x_poly
+        points[i] = x_poly.coeffs
+    return points
+
+def regression_pdl(x, y, method, **config):
+    if config == {}:
+        config = {"lr0": 0.5, "d": 0.005, "epoch": 1000}
+    f = lambda x_poly: (y - x_poly(x.T[0]))
+    jacobi = lambda x_poly: np.array([- x_poly.grad(x[i]) for i in range(len(x))])
+    hessian = lambda x_poly: np.matmul(jacobi(x_poly).T, jacobi(x_poly))
+    grad = lambda x_poly: 2*np.matmul(jacobi(x_poly).T, f(x_poly))
+    bs = method(f, grad, hessian, np.zeros(len(x)), **config)
+    # print('hm')
+    # print(f'came close by {f(Poly(coeffs=bs[-1]))}')
+    return bs[-1]
+
+def test_pdl(coeffs, points, **config):
+    coeffs = np.array(coeffs)
+    points = np.array(points)
+    test_poly = Poly(coeffs=coeffs)
+    res = regression_pdl(np.array(points.reshape(-1,1)),test_poly(points),trust_region_method, **config)
+    print(f'PDL result for {coeffs} is\n{res}')
+    return res
+
+
+# test_pdl([1, 0, 1], [1, 0, -1], epoch=40, tr0=1)
